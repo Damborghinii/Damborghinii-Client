b6d7fffee7883a72a650f6c7ca4a12d30564e995
sayyyho <323psh@naver.com>
2025-06-12 00:12:39 +0900

    feature/#15: 대출신청정보 카드 UI 구현

--- src/pages/invesetment/InvestmentInfo.tsx
+++ src/pages/invesetment/InvestmentInfo.tsx
@@ -2,16 +2,32 @@ import styled from "@emotion/styled";
 
 import { SubTopBar } from "@components/common/topBar/SubTopBar";
 import { useParams } from "react-router-dom";
-import { LoanInfoCard } from "./_components/LoanInfoCard";
+import { LoanInfoCard, LoanInfoProps } from "./_components/LoanInfoCard";
 
 export const InvestmentInfo = () => {
   const { investmentId } = useParams();
   console.log(investmentId);
+
+  const DUMMY_LOAN: LoanInfoProps = {
+    loanAmount: "1000원",
+    monthlyInterest: "300원",
+    repaymentMethod: "만기상환방식",
+    annualInterestRate: "30%",
+    totalRepaymentAmount: "0원",
+    delinquencyRate: "5%",
+    loanPeriod: "1년",
+    repaymentRounds: "1회차",
+    applicant: {
+      name: "박세호",
+      job: "학생",
+    },
+  };
+
   return (
     <>
       <SubTopBar title={"투자 진행 건"} />
       <Wrapper>
-        <LoanInfoCard />
+        <LoanInfoCard {...DUMMY_LOAN} />
       </Wrapper>
     </>
   );
--- src/pages/invesetment/_components/LoanInfoCard.tsx
+++ src/pages/invesetment/_components/LoanInfoCard.tsx
@@ -5,14 +5,96 @@ import { HorizontalDivider } from "@components/common/horizontalDivider/Horizont
 
 import { InvestmentTitle } from "./InvestmentTitle";
 
-export const LoanInfoCard = () => {
+export interface LoanInfoProps {
+  loanAmount: string;
+  monthlyInterest: string;
+  repaymentMethod: string;
+  annualInterestRate: string;
+  totalRepaymentAmount: string;
+  delinquencyRate: string;
+  loanPeriod: string;
+  repaymentRounds: string;
+  applicant: {
+    name: string;
+    job: string;
+  };
+}
+
+export const LoanInfoCard = (props: LoanInfoProps) => {
   return (
     <Wrapper>
       <InvestmentTitle text="대출 신청 정보" />
       <InfoWrapper>
+        <RowWrapper>
+          <CardTextWrapper>
+            <CardMainTitle>대출액</CardMainTitle>
+            <CardMainContent>{props.loanAmount}</CardMainContent>
+          </CardTextWrapper>
+          <CardTextWrapper>
+            <CardMainTitle>월 이자</CardMainTitle>
+            <CardMainContent>{props.monthlyInterest}</CardMainContent>
+          </CardTextWrapper>
+        </RowWrapper>
+        <Spacer height="0.75rem" />
+        <HorizontalDivider />
+
+        <Spacer height="0.75rem" />
+
+        <RowWrapper>
+          <CardTextWrapper>
+            <CardSubTitle>대출 방식</CardSubTitle>
+            <CardSubContent>{props.repaymentMethod}</CardSubContent>
+          </CardTextWrapper>
+          <CardTextWrapper>
+            <CardSubTitle>연이율</CardSubTitle>
+            <CardSubContent>{props.annualInterestRate}</CardSubContent>
+          </CardTextWrapper>
+        </RowWrapper>
+
+        <Spacer height="0.75rem" />
+
+        <RowWrapper>
+          <CardTextWrapper>
+            <CardSubTitle>총 납부금액</CardSubTitle>
+            <CardSubContent>{props.totalRepaymentAmount}</CardSubContent>
+          </CardTextWrapper>
+          <CardTextWrapper>
+            <CardSubTitle>연체율</CardSubTitle>
+            <CardSubContent>{props.delinquencyRate}</CardSubContent>
+          </CardTextWrapper>
+        </RowWrapper>
+
         <Spacer height="0.75rem" />
         <HorizontalDivider />
         <Spacer height="0.75rem" />
+
+        <RowWrapper>
+          <CardTextWrapper>
+            <CardSubTitle>대출기간</CardSubTitle>
+            <CardSubContent>{props.loanPeriod}</CardSubContent>
+          </CardTextWrapper>
+          <CardTextWrapper>
+            <CardSubTitle>납부회차</CardSubTitle>
+            <CardSubContent>{props.repaymentRounds}</CardSubContent>
+          </CardTextWrapper>
+        </RowWrapper>
+
+        <Spacer height="0.75rem" />
+        <HorizontalDivider />
+        <Spacer height="0.75rem" />
+
+        <ApplicantWrapper>
+          <CardSubTitle>신청자</CardSubTitle>
+          <ApplicantDetailInfo>
+            <CardSubTitle>이름</CardSubTitle>
+            <Spacer height="100%" width="0.5rem" />
+            <CardSubContent>{props.applicant.name}</CardSubContent>
+            <Spacer height="100%" width="1.25rem" />
+            <CardSubTitle>직업</CardSubTitle>
+            <Spacer height="100%" width="0.5rem" />
+            <CardSubContent>{props.applicant.job}</CardSubContent>
+          </ApplicantDetailInfo>
+        </ApplicantWrapper>
       </InfoWrapper>
     </Wrapper>
   );
@@ -29,9 +111,56 @@ const Wrapper = styled.div`
 
 const InfoWrapper = styled.div``;
 
-const MainTextWrapper = styled.div`
+const ApplicantWrapper = styled.div`
+  width: 100%;
   display: flex;
-  gap: 2.5rem;
+  align-items: center;
+  gap: 0.5rem;
 `;
 
-// const;
+const ApplicantDetailInfo = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.675rem;
+  flex: 1;
+  background-color: ${({ theme }) => theme.color.neutral.B00};
+  border-radius: 0.375rem;
+`;
+
+const CardSubTitle = styled.h2`
+  ${({ theme }) => theme.typography["small1-3"]};
+  color: ${({ theme }) => theme.color.neutral.B40};
+`;
+
+const CardSubContent = styled.h2`
+  ${({ theme }) => theme.typography["small1-2"]};
+  color: ${({ theme }) => theme.color.neutral.B70};
+`;
+
+const CardTextWrapper = styled.div`
+  width: 45%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const RowWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+
+  gap: 10%;
+  padding-right: 2rem;
+`;
+
+const CardMainTitle = styled.h1`
+  ${({ theme }) => theme.typography["body2-3"]};
+
+  color: ${({ theme }) => theme.color.neutral.B40};
+`;
+
+const CardMainContent = styled.h1`
+  ${({ theme }) => theme.typography["body2-2"]};
+  color: ${({ theme }) => theme.color.primary.P60};
+`;
