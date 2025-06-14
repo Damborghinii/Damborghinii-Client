9f630c079844985ae6d70dae7ce26626f1ebc743
kimjj0 <bbbrian1028@naver.com>
2025-06-14 22:52:22 +0900

    feature/#24-login-api: 로그인 회원가입 api 연결

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,9 +1,24 @@
-// src/apis/axios.ts
 import axios from "axios";
 import { BASE_URL } from "@constants/path";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,9 +1,24 @@
 import axiosInstance from "@apis/axios";
 import { API_PATH } from "@constants/path";
+import { LoginResponse, SignUpResponse } from "src/types/user";
+import { SignUpFormData } from "src/contexts/SignUpFormContext";
 
 export const checkDuplicateId = async (loginId: string) => {
-  const response = await axiosInstance.get(API_PATH.AUTH.CHECK_ID, {
-    params: { loginId },
+  const response = await axiosInstance.post(API_PATH.AUTH.CHECK_ID, {
+    loginId,
   });
+  return response.data;
+};
+
+export const signUp = async (data: SignUpFormData): Promise<SignUpResponse> => {
+  const response = await axiosInstance.post(API_PATH.AUTH.SIGN_UP, data);
+  return response.data;
+};
+
+export const login = async (data: {
+  loginId: string;
+  password: string;
+}): Promise<LoginResponse> => {
+  const response = await axiosInstance.post(API_PATH.AUTH.LOGIN, data);
   return response.data;
 };
