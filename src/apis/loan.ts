4a96230ecb8d6e6a0d20cc324e2eb9fc62ca9818
sayyyho <323psh@naver.com>
2025-08-29 11:34:53 +0900

    fix

--- src/apis/loan.ts
+++ src/apis/loan.ts
@@ -36,6 +36,15 @@ export type LoanConditionConfirm = {
   repaymentCount: string;
 };
 
+// // 실제 응답 구조
+// interface Copyright {
+//   title: string;           // "똥마려"
+//   singers: string;         // "고추"
+//   isRegistered: string;    // "저작권이 등록되어 있는 음원"
+//   wonPrice: string;        // "15,395,320원"
+//   streamingUrls: string;   // "https://open.spotify.com/track/123456789"
+// }
+
 export const getLoanInformation = async (contractId: number) => {
   const url = API_PATH.LOAN.LOAN_INFO.replace(
     "{contractId}",
@@ -70,8 +79,8 @@ export const getLoanConfirm = async (
   count: number
 ): Promise<
   BaseResponse<{
+    copyright: CopyrightDetail;
     loanCondition: LoanConditionConfirm;
-    copyright: Copyright;
   }>
 > => {
   try {
--- src/pages/myNft/_components/myNftContract/ContractCondition.tsx
+++ src/pages/myNft/_components/myNftContract/ContractCondition.tsx
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import theme from "@styles/theme";
-import React from "react";
 
 type InfoField = {
   label: string;
